cc7cfeae2d8dcb50bb2deff7974eabfb707e3a4f
aryanarora180 <aryan.arora180@gmail.com>
2021-01-08 06:28:36 +0530

    Added the UI and adapter for Todos

---
+++ app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.todo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.databinding.ItemRowTodoBinding
+import com.example.todo.db.Todo
+
+class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+
+    lateinit var deleteListener: () -> Unit
+
+    var data = listOf<Todo>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemRowTodoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val todo = data[position]
+
+        with(holder.binding) {
+            titleText.text = todo.title
+            descriptionText.text = todo.description
+
+            deleteImage.setOnClickListener { deleteListener }
+        }
+    }
+
+    override fun getItemCount() = data.size
+
+    inner class ViewHolder(val binding: ItemRowTodoBinding) : RecyclerView.ViewHolder(binding.root)
+}
